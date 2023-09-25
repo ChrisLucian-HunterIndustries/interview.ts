ca115ebabc14e23de56f8042e157efc2d1139c05
Chris Lucian <chris.lucian@hunterindustries.com>
2023-09-25 13:56:17 -0700

    F initial code

---
+++ src/PhoneNumber.ts
@@ -0,0 +1,24 @@
+export class PhoneNumber {
+  
+    constructor(originalValue: string) {
+
+    }
+  
+    getOriginalText(): string {
+      return "";
+    }
+  
+    getStrippedNumber(): string {
+      return "";
+    }
+  
+    getValueAsNorthAmerican(): string {
+      return "";
+    }
+  
+    getValueAsInternational(): string {
+      return "";
+    }
+  
+  }
+  
